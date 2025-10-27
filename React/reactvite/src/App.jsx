f675aca8e7356835dc5967fddb9a25cf5933d5a5
Ansh-699 <anshtyagi7845@gmail.com>
2025-10-27 13:05:34 +0530

    27-oct

---
+++ React/reactvite/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Profile from './component/Profile'
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+        This is abes
+        <Profile/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ React/reactvite/src/component/Profile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <div>THIS IS MY Profile</div>
+  )
+}
+
+export default Profile
